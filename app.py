e52dd522c096e507a7daa6ca05bfa64660811824
Chinmayee Maharana <maharanachinmayee1@gmail.com>
2025-09-28 16:18:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import calendar
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import openpyxl
+
 
 st.title("📊 Sales Prediction & Presentation")
 
